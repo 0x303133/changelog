2bb20e15f72c231cd59c13363e5dc54079cd7e97
0x303133 <0x303133@proton.me>
2023-07-04 13:01:57 +0300

    feat: Add some checks and parse token from config

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,12 @@
-import yargs from "yargs"
+import process from "process";
+import fs from "fs/promises";
+import yargs from "yargs";
+import path from "path";
 
 import { Git } from "./git";
+import { cwd } from "./utils";
 
-const UNRELEASED = "unreleased"
+const UNRELEASED = "unreleased";
 
 export async function run() {
   const argv = await yargs
@@ -12,12 +16,12 @@ export async function run() {
         type: "string",
         desc: "A git tag or commit hash that determines the lower bound of the range of commits",
         defaultDescription: "latest tagged commit",
-        default: Git.tags.first()
+        default: Git.tags.first(),
       },
       to: {
         type: "string",
         desc: "A git tag or commit hash that determines the upper bound of the range of commits",
-        default: Git.tags.head()
+        default: Git.tags.head(),
       },
       "next-version": {
         type: "string",
@@ -27,23 +31,37 @@ export async function run() {
       "contributors-marker": {
         type: "string",
         desc: "Contributors marker. Default: -",
-        default: "-"
+        default: "-",
       },
       "commits-marker": {
         type: "string",
         desc: "Commits marker. Default: *",
-        default: "*"
-      }
+        default: "*",
+      },
     })
     .example(
       "changelog",
-      'Create a changelog for the changes after the latest available tag, under "Unreleased" section'
+      "Create a changelog for the changes after the latest available tag, under \"Unreleased\" section",
     )
     .example(
       "changelog --from=v0.1.0 --to=v0.3.0",
-      "Create a changelog for the changes in all tags within the given range"
+      "Create a changelog for the changes in all tags within the given range",
     )
     .epilog("For more information, see https://github.com/0x303133/changelog")
     .wrap(Math.min(100, yargs.terminalWidth()))
     .parse();
+
+  if (!argv.from) {
+    console.log(`[Changelog]: First tag doesn't exist. Please, create initial tag.`);
+    process.exit();
+  }
+
+  try {
+    await fs.access(path.join(cwd, ".changelogc"));
+  } catch (e) {
+    console.log(`[Changelog]: Config file doesn't exist: (.changelogc)`);
+    process.exit();
+  }
+
+  const token = (await fs.readFile(path.join(cwd, ".changelogc"))).toString();
 }
