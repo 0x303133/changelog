5c2916e8268c00dce8b89df129fd803283acd02d
0x303133 <0x303133@proton.me>
2023-07-04 12:30:04 +0300

    feat: Add new options to cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,39 @@
 import yargs from "yargs"
 
+import { Git } from "./git";
+
 const UNRELEASED = "unreleased"
 
 export async function run() {
-  const argv = yargs
+  const argv = await yargs
     .usage("changelog [options]")
     .options({
       from: {
         type: "string",
         desc: "A git tag or commit hash that determines the lower bound of the range of commits",
         defaultDescription: "latest tagged commit",
+        default: Git.tags.first()
       },
       to: {
         type: "string",
         desc: "A git tag or commit hash that determines the upper bound of the range of commits",
+        default: Git.tags.head()
       },
       "next-version": {
         type: "string",
         desc: "The name of the next version",
         default: UNRELEASED,
       },
+      "contributors-marker": {
+        type: "string",
+        desc: "Contributors marker. Default: -",
+        default: "-"
+      },
+      "commits-marker": {
+        type: "string",
+        desc: "Commits marker. Default: *",
+        default: "*"
+      }
     })
     .example(
       "changelog",
