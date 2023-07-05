682a7743fbc4417a2aeb6462abbbea46558859b2
0x303133 <0x303133@proton.me>
2023-07-05 12:05:16 +0300

    feat: Create ConfigFactory

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
+import hostedInfo from "hosted-git-info";
+import process from "process";
+
 type BaseArgv = {
   to: string
   from: string
@@ -8,10 +11,70 @@ type BaseArgv = {
 
 type Pkg = {
   repo?: { url: string }
-  repository: { url: string }
+  repository?: { url: string }
   labels?: Record<string, string>
+  ignoreContributors?: string[]
 }
 
+type Config = BaseArgv & {
+  repository: string,
+  labels: Record<string, string>
+  ignoreContributors: string[]
+  token: string
+}
+
+const defaultIgnore = [
+  "dependabot-bot",
+  "dependabot[bot]",
+  "dependabot-preview[bot]",
+  "greenkeeperio-bot",
+  "greenkeeper[bot]",
+  "renovate-bot",
+  "renovate[bot]",
+];
+
+const defaultLabels = {
+  breaking: ":boom: Breaking Change",
+  enhancement: ":rocket: Enhancement",
+  bug: ":bug: Bug Fix",
+  documentation: ":memo: Documentation",
+  internal: ":house: Internal",
+};
+
 export class ConfigFactory {
-  public static build(argv: BaseArgv, pkg: Pkg, token: string) {}
+  public static build(argv: BaseArgv, pkg: Pkg, token: string): Config {
+    return {
+      to: argv.to,
+      from: argv.from,
+      token: token,
+      labels: ConfigFactory.labels(pkg),
+      repository: ConfigFactory.repository(pkg),
+      ignoreContributors: ConfigFactory.ignore(pkg),
+      "next-version": argv["next-version"],
+      "commits-marker": argv["commits-marker"],
+      "contributors-marker": argv["contributors-marker"],
+    };
+  }
+
+  private static repository(pkg: Pkg) {
+    const repo = pkg.repo ? pkg.repo : pkg.repository as { url: string };
+    if (!repo) {
+      console.log(`[Changelog]: Couldn't found git repository. Please, provide it in package.json`);
+      process.exit();
+    }
+    const info = hostedInfo.fromUrl(repo.url);
+    if (!info) {
+      console.log(`[Changelog]: Couldn't found any info about git repository. Please, check if you provide correct repo in package.json`);
+      process.exit();
+    }
+    return `${info.user}/${info.project}`;
+  }
+
+  private static ignore(pkg: Pkg) {
+    return "ignoreContributors" in pkg && pkg.ignoreContributors ? pkg.ignoreContributors : defaultIgnore;
+  }
+
+  private static labels(pkg: Pkg) {
+    return "labels" in pkg && pkg.labels ? pkg.labels : defaultLabels;
+  }
 }
